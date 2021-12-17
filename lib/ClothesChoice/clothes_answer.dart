5c4ffe2dbfcd53c6f3b2d077abb581bde8a26b8e
FlutLab.io <>
2021-12-17 12:36:38 +0000

    Initial commit

---
+++ lib/ClothesChoice/clothes_answer.dart
@@ -0,0 +1,27 @@
+// import 'package:flutter/material.dart';
+
+// class ClothesAnswer extends StatelessWidget {
+//   String answerText;
+//   Function tapped;
+//   ClothesAnswer(this.tapped, this.answerText);
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Container(
+//       height: 50,
+//       width: 150,
+//       margin: const EdgeInsets.only(top: 10.0),
+//       child: new ElevatedButton(
+//           child: Text(
+//             answerText,
+//             style: TextStyle(
+//               color: Colors.red,
+//               fontSize: 20,
+//             ),
+//           ),
+//           style: ButtonStyle(
+//             backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+//           ),
+//           onPressed: tapped),
+//     );
+//   }
+// }
