6d23471b1e5ed180935d73eb9485dfa601fbbf74
Viktorija2Nikolovska <63191724+Viktorija2Nikolovska@users.noreply.github.com>
2021-12-17 13:55:21 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,81 @@
 import 'package:flutter/material.dart';
 
-import 'Moneye/moneye_home.dart';
+import 'ClothesChoice/clothes_questions.dart';
+import 'ClothesChoice/clothes_answer.dart';
+import 'dart:convert';
+import 'dart:core';
 
-void main() {
-  runApp(MyApp());
+// опчињата да бидат со зелена позадина и текстот да биде црвен,
+// додека текстот на прашањето да биде син.
+// Направете копчињата да бидат разделени
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var questions = [
+    {
+      "question": "Select top",
+      'answer': [
+        'Shirt',
+        'T-Shirt',
+        'Jacket',
+        'Jumper'
+      ]
+    },
+    {
+      "question": "Select bottom",
+      'answer': [
+        'Jeans',
+        'Pants',
+        'Skirt'
+      ]
+    },
+    {
+      "question": "Select shoes",
+      'answer': [
+        'Sneakers',
+        'Boots',
+        'Heels'
+      ]
+    }
+  ];
+  var _questionIndex = 0;
+
+  void _iWasTapped() {
+    if (_questionIndex == questions.length - 1) {
+      _questionIndex = -1;
+      return;
+    }
+    setState(() {
+      _questionIndex += 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Moneye',
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: Moneye(),
+      
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Choose clothes",
+          ),
+        ),
+        body: Column(
+          children: [
+            ClothesQuestion(questions[_questionIndex]['question']),
+            ...(questions[_questionIndex]['answer'] as List<String>).map((answer) {
+              return ClothesAnswer(_iWasTapped, answer);
+            }),
+          ],
+        ),
+      ),
     );
   }
 }
